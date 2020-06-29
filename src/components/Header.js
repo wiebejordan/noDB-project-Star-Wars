785ec45fd30ab1035983fa018e7b095a726a926d
wiebejordan <wiebe.jordan@yahoo.com>
2020-06-29 11:42:46 -0600

    ready for presentation

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return(
     <div className='header'>
-      <h1>Imperial Fleet Inbound!</h1>
+      <h1>TIE Fighters Incoming!</h1>
       <h1>Launch our starfighters!</h1>
     </div>
   )
